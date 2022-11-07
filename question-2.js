7c3d0aed2464d8bfd058f8adfaa9e81fa50e2385
David R <99845204+NORtacus@users.noreply.github.com>
2022-11-07 20:00:30 +0100

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,11 +1,11 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b701b727e5a544419c0f3c40a7c3132f";
 
-const resultsContainer = document.querySelector(".results");
+const resultContainer = document.querySelector(".gameList");
 
 function createHTML(results) {
     const games = results.all;
 
-    resultsContainer.innerHTML = "";
+    gameListContainer.innerHTML = "";
 
         for (let i = 0; i < games.length; i++) {
 
@@ -13,6 +13,29 @@ function createHTML(results) {
                 break;
             }
 
-            resultsContainer.innerHTML += `<div class="result">${games[i].text}</div>`;
+            gameListContainer.innerHTML += `<div class="result">${games[i].text}</div>`;
         }
+}
+
+
+async function getGames() {
+    try {
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        const facts = data.all;
+
+        createHTML(facts);
+
+    } catch (error) {
+        gameListContainer.innerHTML = displayError("An error occurred when calling the API");
+    }
+}
+
+getGames();
+
+//displayErro.js//
+function displayError(message = "Unknown error") {
+    return `<div class="error">${message}</div>`;
 }
