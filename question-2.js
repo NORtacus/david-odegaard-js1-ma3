563ee11e0b63064d83b2c46c10cddd7b8e7e1257
David R <99845204+NORtacus@users.noreply.github.com>
2022-11-07 06:13:58 +0100

    Update question-2.js

--- question-2.js
+++ question-2.js
@@ -1,1 +1,18 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b701b727e5a544419c0f3c40a7c3132f";
+
+const resultsContainer = document.querySelector(".results");
+
+function createHTML(results) {
+    const games = results.all;
+
+    resultsContainer.innerHTML = "";
+
+        for (let i = 0; i < games.length; i++) {
+
+            if (i === 8) {
+                break;
+            }
+
+            resultsContainer.innerHTML += `<div class="result">${games[i].text}</div>`;
+        }
+}
